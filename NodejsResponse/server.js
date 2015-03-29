d3a2057bb3ef5b39f58fa5303751e80eff5e4618
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-03-29 21:00:56 +0700

    Initial commit

---
+++ NodejsResponse/server.js
@@ -0,0 +1,29 @@
+﻿var http = require('http');
+
+var port = process.env.port || 1337;
+
+var onRequest = function (request, response) {
+    var fs = require('fs');
+    var file = fs.createReadStream('./index.html');
+    var content = '';
+    
+    file.on('data', function (data) {
+        content = content + data;
+    });
+
+    file.on('end', function () {
+        var result = content;
+
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Content-Length': result.length
+        });
+
+        response.write(result);
+        response.end();
+    });
+};
+
+var server = http.createServer(onRequest);
+
+server.listen(port, 'localhost');
